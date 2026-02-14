31986f5c69a1250206de0d308ec4acc167cb1b7f
Aaron <tharuaaron01@gmail.com>
2026-02-14 23:43:38 +0545

    Review routes modified

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/", protect, addReview);
 
-// summary MUST be before :dressId
+
 router.get("/summary/:dressId", getDressRatingSummary);
 router.get("/:dressId", getDressReviews);
 
